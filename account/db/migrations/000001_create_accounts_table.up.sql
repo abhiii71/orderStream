5b2ff99db7aa00c599c49a5d7922ff826828838d
Abhishek <abhishek.work71@gmail.com>
2025-10-08 00:43:26 +0530

    migrations for account table

---
+++ account/db/migrations/000001_create_accounts_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE accounts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
