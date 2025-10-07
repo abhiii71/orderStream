cdd27f1e264e4070a6c73269d4a46fa5748dae3e
Abhishek <abhishek.work71@gmail.com>
2025-10-07 21:09:55 +0530

    something wrong here (have to fixed)

--- account/cmd/account/main.go
+++ account/cmd/account/main.go
@@ -1,1 +1,32 @@
-package main 
+package main
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	"github.com/abhiii71/orderStream/account/config"
+	"github.com/abhiii71/orderStream/account/internal"
+	"github.com/tinrab/retry"
+	"gorm.io/driver/postgres"
+)
+
+func main() {
+	var repo internal.AccountRepository
+
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		db, err := sql.Open(postgres.Open(config.DatabaseURL), &sql.Config{})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		repo := internal.Newrepo(db)
+
+		return
+	})
+
+	defer repo.Close()
+	log.Println("Listening on port 8080...")
+	service := internal.Newservice(repo)
+	log.Fatal(internal.ListenGRPC(service, 8080))
+}
