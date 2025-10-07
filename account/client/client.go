d3550ab3e76718af10e7b3aadbc0dc7974f418bb
Abhishek <abhishek.work71@gmail.com>
2025-10-07 21:09:04 +0530

    client interact with RPC-> then server

--- account/client/client.go
+++ account/client/client.go
@@ -1,1 +1,45 @@
 package client
+
+import (
+	"context"
+	"log"
+
+	"github.com/abhiii71/orderStream/account/proto/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	conn    *grpc.ClientConn
+	service pb.AccountServiceClient
+}
+
+func NewClient(url string) (*Client, error) {
+	conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	C := pb.NewAccountServiceClient(conn)
+	return &Client{conn, C}, nil
+}
+
+func (c *Client) Close() {
+	err := c.conn.Close()
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func (c *Client) Register(ctx context.Context, name, email, password string) (string, error) {
+	response, err := c.service.Register(ctx, &pb.RegisterRequest{
+		Name:     name,
+		Email:    email,
+		Password: password,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return response.Value, nil
+
+}
