125b2f2f3671b23547e991d57f0e425475a2b477
Abhishek <abhishek.work71@gmail.com>
2025-10-11 09:18:16 +0530

    fix(bug)

--- product/models/product.go
+++ product/models/product.go
@@ -3,13 +3,13 @@ package models
 type Product struct {
 	Id         string  `json:"id"`
 	Name       string  `json:"name"`
-	Decription string  `json:"description"`
+	Description string  `json:"description"`
 	Price      float64 `json:"price"`
 	AccountId  int     `json:"accountId"`
 }
 
 type ProductDocument struct {
 	Name       string  `json:"name"`
-	Decription string  `json:"description"`
+	Description string  `json:"description"`
 	Price      float64 `json:"price"`
 }
