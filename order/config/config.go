5abb16dcdc9f2b7d2a787fdb59cbe4fb3bbe3e83
Abhishek <abhishek.work71@gmail.com>
2025-10-11 21:22:34 +0530

    Refactred

--- order/config/config.go
+++ order/config/config.go
@@ -6,12 +6,12 @@ var (
 	DatabaseURL     string
 	AccountURL      string
 	ProductURL      string
-	BootStrapServer string
+	BootStrapServers string
 )
 
 func init() {
 	DatabaseURL = os.Getenv("DATABASE_URL")
 	AccountURL = os.Getenv("ACCOUNT_URL")
 	ProductURL = os.Getenv("PRODUCT_URL")
-	BootStrapServer = os.Getenv("KAFKA_BOOTSTRAP_SERVERS")
+	BootStrapServers = os.Getenv("KAFKA_BOOTSTRAP_SERVERS")
 }
