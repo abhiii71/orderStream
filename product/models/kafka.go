e2b4a2723840b05af13bf4a6cc32d7d6c0bbb6d6
Abhishek <abhishek.work71@gmail.com>
2026-01-28 22:04:28 +0530

    added: products_listed event

--- product/models/kafka.go
+++ product/models/kafka.go
@@ -1,14 +1,25 @@
 package models
 
 type EventData struct {
-	Id          *string  `json:"product_id`
-	Name        *string  `json:"name"`
-	Description *string  `json:"description"`
-	Price       *float64 `json:"price"`
-	AccountID   *int     `json:"accountID"`
+	Id          *string  `json:"product_id,omitempty"`
+	Name        *string  `json:"name,omitempty"`
+	Description *string  `json:"description,omitempty"`
+	Price       *float64 `json:"price,omitempty"`
+	AccountID   *int     `json:"accountID,omitempty"`
 }
 
 type Event struct {
 	Type string    `json:"type"`
 	Data EventData `json:"data"`
+}
+
+// ProductsListedEventData for when multiple products are listed
+type ProductsListedEventData struct {
+	ProductIDs []string `json:"product_ids"`
+	Count      int      `json:"count"`
+}
+
+type ProductsListedEvent struct {
+	Type string                  `json:"type"`
+	Data ProductsListedEventData `json:"data"`
 }
